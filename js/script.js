13ae6023599b0dd26ff08a7127d78458ff1369ef
lucas-nguyen0 <lucas.nguyen@mths.ca>
2025-05-02 17:29:36 +0000

    java and index

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Lucas Nguyen All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lucas Nguyen
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+
+
+function enterClicked() {
+  const inputNumber1 = document.getElementById('input-number-one').value
+  const inputNumber2 = document.getElementById('input-number-two').value
+  let answer = 0
+  while (inputNumber1 >= 1) {
+    answer = answer + inputNumber2
+    inputNumber1--
+  } 
+  document.getElementById("answer").innerHTML =
+    'The answer is: ' + answer + '.'
+}

4139b5684990b3bfac32ac15cdaecec9495b2418
default <lucas.nguyen@mths.ca>
2025-05-06 16:58:54 +0000

    js

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@
 // Created on: Apr 2025
 // This file contains the JS functions for index.html
 
-
+'use strict'
 
 function enterClicked() {
-  const inputNumber1 = document.getElementById('input-number-one').value
-  const inputNumber2 = document.getElementById('input-number-two').value
+  let inputNumber1 = parseFloat(document.getElementById('input-number-one').value)
+  const inputNumber2 = parseFloat(document.getElementById('input-number-two').value)
   let answer = 0
   while (inputNumber1 >= 1) {
     answer = answer + inputNumber2
